49fb17c127636d9ba3514756081b2c95e91b8849
eahme <eahme@EHAB>
2024-02-24 20:42:34 +0200

    Add project files.

---
+++ HealthCare.Core/Specifications/ISpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Core.Specifications
+{
+    public interface ISpecification <T>
+    {
+        public Expression<Func<T,bool>> Criteria { get; set; } 
+
+        public List<Expression<Func<T,object>>> Includes { get; set; } 
+    }
+}
