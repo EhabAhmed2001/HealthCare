8beefa2ec4aca12b1b8fbb191af120c8106474ff
mayar yasser <mayar.yasser9001@gmail.com>
2024-04-07 14:07:34 +0200

    Create Search Method

---
+++ HealthCare.API/Helper/UserHelper.cs
@@ -0,0 +1,28 @@
+﻿using HealthCare.Core.Entities.identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace HealthCare.PL.Helper
+{
+    public class UserHelper
+    {
+        public async Task<string?> UserSearch(string Email, string Role, UserManager<AppUser> userManager)
+        {
+            
+            var user = await userManager.FindByEmailAsync(Email);
+            if (user == null)
+            {
+                return null;
+            }
+
+            // Check if the user has the given role
+           else if (await userManager.IsInRoleAsync(user, Role))
+            {
+                return user.Id;
+            }
+            
+          // If the user does not have the given role
+            return null;
+            
+        }
+    }
+}
