6853331870ff33be86692775999938bfbacf3659
Engy Saad <engyabdlrhman@gmail.com>
2024-04-27 02:08:34 +0300

    Add Request

---
+++ HealthCare.Core/Entities/Data/AddRequest/NotificationStatus.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+[JsonConverter(typeof(StringEnumConverter))]
+public enum NotificationStatus
+{
+    [EnumMember(Value = "Pending")]
+    Pending,
+
+    [EnumMember(Value = "Approved")]
+    Approved,
+
+    [EnumMember(Value = "Rejected")]
+    Rejected
+}
