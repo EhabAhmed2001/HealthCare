2b9172dde82d86375bae2a81c26e24591b0a3494
Fanko <161323611+FankoSaad@users.noreply.github.com>
2024-04-27 03:38:50 +0300

    Add files via upload
    Add Notifications

---
+++ HealthCare.Core/Entities/Data/Notifications/Notifications.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HealthCare.Core.AddRequest;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using HealthCare.Core.Entities.identity;
+
+namespace HealthCare.Core.AddRequest
+{
+    public class Notification
+    {
+        public int SenderId { get; set; }
+        public AppUser Sender { get; set; }
+        public int ReceiverId { get; set; }
+        public AppUser Receiver { get; set; }
+        public string SenderEmail { get; set; }
+        public string ReceiverEmail { get; set; }
+        public NotificationStatus Status { get; set; } = NotificationStatus.Pending;
+    }
+}
