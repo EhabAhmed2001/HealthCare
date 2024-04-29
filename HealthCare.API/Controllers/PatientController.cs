86a314e500b9334167e9cd3f644228abb7ad128f
mayar yasser <mayar.yasser9001@gmail.com>
2024-04-29 20:32:21 +0300

    Add the Data of patient's doctor and observer Endpoint

--- HealthCare.API/Controllers/PatientController.cs
+++ HealthCare.API/Controllers/PatientController.cs
@@ -12,6 +12,9 @@ using Microsoft.AspNetCore.Identity;
 using HealthCare.Core.Entities.identity;
 using HealthCare.Core.Services;
 using HealthCare.Core.Entities;
+using HealthCare.PL.DTOs;
+using System.Security.Claims;
+using HealthCare.Repository.Data;
 
 namespace HealthCare.PL.Controllers
 {
@@ -22,13 +25,15 @@ namespace HealthCare.PL.Controllers
         [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "<Pending>")]
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly ILogger<PatientController> logger;
+        private readonly HealthCareContext _dbContext;
 
-        public PatientController(UserManager<AppUser> userManager, ITokenService token, RoleManager<IdentityRole> roleManager, ILogger<PatientController> logger)
+        public PatientController(UserManager<AppUser> userManager, ITokenService token, RoleManager<IdentityRole> roleManager, ILogger<PatientController> logger, HealthCareContext dbContext)
         {
             _userManager = userManager;
             _token = token;
             _roleManager = roleManager;
             this.logger = logger;
+            _dbContext = dbContext;
         }
         /*
                 [HttpPost("PatientRegister")]
@@ -84,5 +89,82 @@ namespace HealthCare.PL.Controllers
                         return BadRequest(Result.Errors);
                     }
     }*/
+
+
+        [HttpGet("GetDoctorData")]
+        public ActionResult<UserDto> GetDoctorData()
+        {
+            // Get the patient's email from the user's claims
+            var patientEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            if (string.IsNullOrEmpty(patientEmail))
+            {
+                return BadRequest("Patient not found.");
+            }
+
+            // Get the patientdoctorID from the database
+            var patient = (Patient)_userManager.FindByEmailAsync(patientEmail).Result;
+
+            // Get the doctor data from the database
+            var doctorData = _dbContext.Doctor.FirstOrDefault(d => d.Id == patient.PatientDoctorId);
+
+            if (doctorData == null)
+            {
+                return NotFound("Doctor data not found.");
+            }
+
+            var doctorDto = new UserDto
+            {
+                Email = doctorData.Email,
+                UserName = doctorData.UserName,
+                FisrtName = doctorData.FirstName,
+                LastName = doctorData.LastName,
+                Address = doctorData.Address,
+                PhoneNamber = doctorData.PhoneNumber,
+                PictureUrl = doctorData.PictureUrl,
+            };
+            return Ok(doctorDto);
+        }
+
+
+
+
+
+        [HttpGet("GetObserverData")]
+        public ActionResult<UserDto> GetObserverData()
+        {
+            // Get the patient's email from the user's claims
+            var patientEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            if (string.IsNullOrEmpty(patientEmail))
+            {
+                return BadRequest("Patient not found.");
+            }
+
+            // Get the patient's ID from the database
+            var patient = _userManager.FindByEmailAsync(patientEmail).Result;
+
+
+            // Get the observer data from the database
+            var observerData = _dbContext.Observer.FirstOrDefault(o => o.PatientObserverId == patient.Id);
+
+            if (observerData == null)
+            {
+                return NotFound("Observer data not found.");
+            }
+
+            var observerDto = new UserDto
+            {
+                Email = observerData.Email,
+                UserName = observerData.UserName,
+                FisrtName = observerData.FirstName,
+                LastName = observerData.LastName,
+                Address = observerData.Address,
+                PhoneNamber = observerData.PhoneNumber,
+                PictureUrl = observerData.PictureUrl,
+            };
+
+            return Ok(observerDto);
+        }
     }
 }
