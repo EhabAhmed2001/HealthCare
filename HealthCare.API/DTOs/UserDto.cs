d7e118c896836218d85630866770791ce8ee9259
EhabAhmed <e.ahmed2684@gmail.com>
2024-06-13 16:06:07 +0300

    Edit User Dto

--- HealthCare.API/DTOs/UserDto.cs
+++ HealthCare.API/DTOs/UserDto.cs
@@ -4,8 +4,6 @@ namespace HealthCare.PL.DTOs
 {
     public abstract class UserDto : UserSearchToReturnDto
     {
-        public string Email { get; set; }
-
         public Address Address { get; set; }
 
         public string PhoneNumber { get; set; }
